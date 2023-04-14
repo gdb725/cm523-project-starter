751e2aa345c9e08289b52a83f0679a367371d637
Graceb725 <gdb725@bu.edu>
2023-04-14 10:41:51 -0400

    addedJSSmap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
+var map = L.map('map').setView([42.3601, -71.0589], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+}).addTo(map);
+
+var marker = L.marker([42.3432517, -71.1227484]).addTo(map);
+
+marker.bindPopup("<b>Boston General Store</b><br> 305 Harvard St Brookline, MA 02446").openPopup();
+
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
