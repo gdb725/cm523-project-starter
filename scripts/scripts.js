47cb2ec7279e0b531486230614351921b8696a95
Graceb725 <gdb725@bu.edu>
2023-05-02 16:34:16 -0400

    Final touches

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,47 +27,54 @@
   window.addEventListener("scroll", callbackFunc);
 })();
 
-var map = L.map('map').setView([42.3601, -71.0589], 13);
+map = L.map('map').setView([42.3601, -71.0589], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 }).addTo(map);
 
-var marker = L.marker([42.3432517, -71.1227484]).addTo(map);
+marker = L.marker([42.3432517, -71.1227484]).addTo(map);
 marker.bindPopup("<b>Boston General Store</b><br> 305 Harvard St Brookline, MA 02446").openPopup();
 
-var marker = L.marker([42.3432517, -71.1227484]).addTo(map);
+marker = L.marker([42.3432517, -71.1227484]).addTo(map);
 marker.bindPopup("<b>VomFass</b><br> 305 Harvard St Brookline, MA 02446").openPopup();
 
-var marker = L.marker([42.381230, -71.174630]).addTo(map);
+marker = L.marker([42.381230, -71.174630]).addTo(map);
 marker.bindPopup("<b>The Spirited Gourmet</b><br> 448 Common St Belmont, MA 02478").openPopup();
 
-var marker = L.marker([42.405910, -71.143150]).addTo(map);
+marker = L.marker([42.405910, -71.143150]).addTo(map);
 marker.bindPopup("<b>Yes! Plastic Free!</b><br>212 Massachusetts Ave Arlington, MA 02474").openPopup();
 
-var marker = L.marker([42.362280, -71.080310]).addTo(map);
+marker = L.marker([42.362280, -71.080310]).addTo(map);
 marker.bindPopup("<b>Brother's MarketPlace</b><br>1 Broadway St Cambridge, MA 02142").openPopup();
 
-var marker = L.marker([42.389690, -71.118190]).addTo(map);
+marker = L.marker([42.389690, -71.118190]).addTo(map);
 marker.bindPopup("<b>Cambridge Naturals</b><br>23 White St Cambridge, MA 02140").openPopup();
 
-var marker = L.marker([42.366460, -71.100660]).addTo(map);
+marker = L.marker([42.366460, -71.100660]).addTo(map);
 marker.bindPopup("<b>Cleenland</b><br>89A Norfolk St Cambridge, MA 02139").openPopup();
 
-var marker = L.marker([42.3942643, -71.1269294]).addTo(map);
+marker = L.marker([42.3942643, -71.1269294]).addTo(map);
 marker.bindPopup("<b>Curio Spice</b><br>2265 Massachusetts Ave Cambridge, MA 02140").openPopup();
 
-var marker = L.marker([42.3939195, -71.1254085]).addTo(map);
+marker = L.marker([42.3939195, -71.1254085]).addTo(map);
 marker.bindPopup("<b>Pemberton Market</b><br>2225 Massachusetts Ave Cambridge, MA 02140").openPopup();
 
-var marker = L.marker([42.3169359, -71.1057952]).addTo(map);
+marker = L.marker([42.3169359, -71.1057952]).addTo(map);
 marker.bindPopup("<b>City Feed and Supply</b><br>66a Boylston St Jamaica Plain, MA 02130").openPopup();
 
-var marker = L.marker([42.4117184, -71.1218144]).addTo(map);
+marker = L.marker([42.4117184, -71.1218144]).addTo(map);
 marker.bindPopup("<b>Trove Green Provisions</b><br>330 Boston Ave Medford, MA 02155").openPopup();
 
-var marker = L.marker([42.3527806, -71.2017338]).addTo(map);
+marker = L.marker([42.3527806, -71.2017338]).addTo(map);
 marker.bindPopup("<b>Fulfilled Goods</b><br>612 Washington St Newton, MA 02458").openPopup();
 
-var marker = L.marker([42.3989548, -71.1091156]).addTo(map);
+marker = L.marker([42.3989548, -71.1091156]).addTo(map);
 marker.bindPopup("<b>Neighborhood Produce</b><br>691 Broadway Somerville, MA 02145").openPopup();
+
+marker = L.marker([42.31299, -71.074767]).addTo(map);
+marker.bindPopup("<b>Supply Bulk Foods</b><br>196 Quincy St, Boston, MA 02122").openPopup();
+
+
+
+
